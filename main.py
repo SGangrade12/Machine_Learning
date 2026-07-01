bdece925780dd9f491d5c0cc7af205e64c0ca7ed
SGangrade12 <sadankshzz@gmail.com>
2026-07-01 10:16:04 +0530

    Python_C1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math as m
+# import math as m
+#
+# x=10.45
+#
+# print(m.ceil(x))
+# print(m.floor(x))
+#
+# print(m.fmod(11,5))
 
-x=10.45
-
-print(m.ceil(x))
-print(m.floor(x))
-
-print(m.fmod(11,5))
+# print(80,90,sep=" OK ",end=" cutu ")
