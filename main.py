a398c10ec6617ec3083b5028f3e0e8c1f6714eca
SGangrade12 <sadankshzz@gmail.com>
2026-07-19 18:03:45 +0530

    Mini Project

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-# import math as m
+# # String Handling
 #
-# x=10.45
 #
-# print(m.ceil(x))
-# print(m.floor(x))
+# s="Hello World"
 #
-# print(m.fmod(11,5))
+# print(s)
+#
+# print(type(s))
+#
+# print(len(s))
+#
+# print(s.lower())
+# print(s.upper())
+# print(s.replace("o","x"))
+# print(s.index("o"))
+# print(s.count("o"))
+# print(s.find("o"))
+# print(s.find("x"))
+# print(s.index("o"))
+# # print(s.index("x"))
+# x=s.split()
+# print(s.split())
+# print(s.split("o"))
+# sep=" "
+# sep.join(x)
+# print(sep.join(x))
+# print(s[3:8:4])
+# print(s[-1:-12:-1])
+# print(s[::-1])
+# print(reversed(s[::-1]))
 
-# print(80,90,sep=" OK ",end=" cutu ")
+
+
+
+# def fact(n):
+#     if n == 0:
+#         return 1
+#     return n * fact(n-1)
+
+# Lambda function
+# Anonymous function
+# Single line function
+# Faster than def function
+# No use of print or return statement
+
+l1=[1,2,3,4,5]
+l2=[10,20,30,40,50]
+
+a=lambda x,y: x if x>y else y
+
+print(a(l1,l2))
+
+
+# Lambda with Map:
---
+++ mini_project.py
@@ -0,0 +1,43 @@
+shipment_Id=int(input("Enter your Shipment ID:"))
+ship_details={}
+def ship_det(shipment_id):
+    while shipment_id!=0:
+        sender=int(input("Enter Sender:"))
+        receiver=int(input("Enter Receiver:"))
+        start_date=input("Enter Start Date:")
+        delivery_date=input("Enter Delivery Date:")
+        sender_location=input("Enter sender location:")
+        receiver_location=input("Enter receiver location:")
+        delivery_status=input("Enter delivery status:")
+        shipping_cost=int(input("Enter shipping cost:"))
+        li=[str(sender),str(receiver),start_date,delivery_date,sender_location,receiver_location,delivery_status,shipping_cost]
+
+        ship_details[shipment_id]=li
+        shipment_id = int(input("Enter your Shipment ID:"))
+
+
+def client_det(client_id):
+    while client_id!=0:
+        client_name=input("Enter Client Name:")
+        client_details[client_id]=client_name
+        client_id=int(input("Enter your Client ID:"))
+
+
+def change_id_to_name(ship_details,client_details):
+    for li in ship_details:
+        if li[1] in client_details:
+            ship_details[li[1]]=client_details[li[1]]
+
+ship_det(shipment_Id)
+print(ship_details)
+
+
+client_id=int(input("Enter your Client ID:"))
+client_details={}
+
+client_det(client_id)
+
+print(client_details)
+
+change_id_to_name(ship_details,client_details)
+
